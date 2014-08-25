8c1aa05b8ee411eaec116b40efeb8fe86fda95e3
Dhruvil Patel <dd.droid@yahoo.com>
2014-08-25 16:08:24 +0530

    jhgjhgjhgjhg

--- app/src/main/java/wm/com/dt/login/LoginActivity.java
+++ app/src/main/java/wm/com/dt/login/LoginActivity.java
@@ -1,21 +1,17 @@
 package wm.com.dt.login;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
 import wm.com.app.BaseActivity;
 import wm.com.dt.R;
 import wm.com.dt.my_plays.MyPlays;
 
-import android.view.View;
-import android.widget.TextView;
-
 public class LoginActivity extends BaseActivity{
+
+    // Added comment by dhruvil
 
     private TextView txtLogin;
     @Override
