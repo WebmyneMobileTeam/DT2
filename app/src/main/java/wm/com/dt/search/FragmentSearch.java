d3b200e74e9ee368685c082f76ef6900ca7e870b
Nirav Kalola <niravkalola1991@gmail.com>
2014-08-26 17:41:09 +0530

    nirav changes search

--- app/src/main/java/wm/com/dt/search/FragmentSearch.java
+++ app/src/main/java/wm/com/dt/search/FragmentSearch.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -14,9 +16,11 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+
+import uk.me.lewisdeane.ldialogs.CustomListDialog;
 import wm.com.dt.R;
 
-public class FragmentSearch extends Fragment {
+public class FragmentSearch extends Fragment implements AdapterView.OnItemClickListener {
 
     private ListView filterCategoryList;
     private ArrayList<Integer> imageList = new ArrayList<Integer>();
@@ -30,27 +34,27 @@ public class FragmentSearch extends Fragment {
     public FragmentSearch() {
         // Required empty public constructor
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         imageList.add(R.drawable.ic_number_of_participants);
+
         imageList.add(R.drawable.ic_age);
         imageList.add(R.drawable.ic_music);
         imageList.add(R.drawable.ic_duration);
-
         titleList.add("Medvirkende");
         titleList.add("Alder");
         titleList.add("Musik");
         titleList.add("Varighed");
+
         filterCategoryList = (ListView) view.findViewById(R.id.filterCategoryList);
         filterCategoryList.setAdapter(new FilterCategoryAdapter(getActivity(), titleList, imageList));
+        filterCategoryList.setOnItemClickListener(this);
         return view;
     }
 
@@ -116,5 +120,36 @@ public class FragmentSearch extends Fragment {
 
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-}
+        switch (position) {
+
+            case 0:
+                Log.e("hiii","fddf");
+                String[] items={"Alle (all)","1-5","6-10","11-15","16-20","21-30","30+"};
+                CustomListDialog customListDialog = new CustomListDialog(getActivity());
+
+                customListDialog.setTitle("Medvirkende")
+                .setItems(items);
+
+                customListDialog.show();
+
+                break;
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+        }
+
+    }
+    }
