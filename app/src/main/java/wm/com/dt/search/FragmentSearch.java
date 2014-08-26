56688c685a5f147c94f3981423d048a77983ef2e
Nirav Kalola <niravkalola1991@gmail.com>
2014-08-26 11:12:39 +0530

    nirav changes darawer icon

--- app/src/main/java/wm/com/dt/search/FragmentSearch.java
+++ app/src/main/java/wm/com/dt/search/FragmentSearch.java
@@ -30,30 +30,10 @@ public class FragmentSearch extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+      View view=inflater.inflate(R.layout.fragment_search, container, false);
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
 
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
 
 }
