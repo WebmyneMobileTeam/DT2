d1eb5c91dc0236b3c3343d26790a5e73017882a5
Nirav Kalola <niravkalola1991@gmail.com>
2014-08-27 15:39:48 +0530

    nirav changes my  play home page parsing

--- app/src/main/java/wm/com/dt/my_plays/FragmentMyPlay.java
+++ app/src/main/java/wm/com/dt/my_plays/FragmentMyPlay.java
@@ -2,32 +2,53 @@ package wm.com.dt.my_plays;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.android.volley.VolleyError;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import wm.com.dt.R;
+import wm.com.dt.customviews.HUD;
 import wm.com.dt.customviews.SegmentedGroup;
+import wm.com.dt.customviews.WMTextView;
 import wm.com.dt.model.CallWebService;
 
 
-public class FragmentMyPlay extends Fragment implements RadioGroup.OnCheckedChangeListener{
-
+public class FragmentMyPlay extends Fragment implements RadioGroup.OnCheckedChangeListener {
+    private HUD dialog;
     private SegmentedGroup segmentedGroupPlays;
     private RadioButton rbBestilte;
     private RadioButton rbGennemsyn;
+    private ListView listPlay;
+
+    private ArrayList<Play> playList;
+    private ArrayList<Play> playListForReview = new ArrayList<Play>();
+    private ArrayList<Play> playListForPerform = new ArrayList<Play>();
+    private ArrayList<PlayOrderDetails> playOrderList = new ArrayList<PlayOrderDetails>();
 
     public static FragmentMyPlay newInstance(String param1, String param2) {
         FragmentMyPlay fragment = new FragmentMyPlay();
 
         return fragment;
     }
+
     public FragmentMyPlay() {
         // Required empty public constructor
     }
@@ -43,27 +64,79 @@ public class FragmentMyPlay extends Fragment implements RadioGroup.OnCheckedChan
                              Bundle savedInstanceState) {
 
         View convertView = inflater.inflate(R.layout.fragment_my_play, container, false);
-        segmentedGroupPlays = (SegmentedGroup)convertView.findViewById(R.id.segmentedMyPlays);
-        rbBestilte = (RadioButton)convertView.findViewById(R.id.rbBestilte);
-        rbGennemsyn = (RadioButton)convertView.findViewById(R.id.rbGennemsyn);
-
+        segmentedGroupPlays = (SegmentedGroup) convertView.findViewById(R.id.segmentedMyPlays);
+        rbBestilte = (RadioButton) convertView.findViewById(R.id.rbBestilte);
+        rbGennemsyn = (RadioButton) convertView.findViewById(R.id.rbGennemsyn);
         segmentedGroupPlays.setOnCheckedChangeListener(this);
-
-
+        listPlay = (ListView) convertView.findViewById(R.id.listPlays);
+//        listPlay.setAdapter(new ListPlayAdapter(getActivity(), titleList, imageList));
         return convertView;
     }
 
-
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        dialog = new HUD(getActivity(), android.R.style.Theme_Translucent_NoTitleBar);
+        dialog.title("Mine Stykker");
+        dialog.show();
+        new CallWebService("http://api.danteater.dk/api/MyPlays?UserId=kf", CallWebService.TYPE_JSONARRAY) {
 
+            @Override
+            public void response(String response) {
+                dialog.dismiss();
+                Log.e("json response", response + "");
+                handledataafterresponseVolly(response);
+            }
 
+            @Override
+            public void error(VolleyError error) {
+                dialog.dismissWithStatus(R.drawable.ic_navigation_cancel, "Error");
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }.start();
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
+    public void handledataafterresponseVolly(final String response) {
+        // TODO Auto-generated method stub
+
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Type listType = new TypeToken<List<Play>>() {
+
+                }.getType();
+
+                playList = new GsonBuilder().create().fromJson(
+                        response, listType);
+
+
+                //testing response
+                for (int i = 0; i < playList.size(); i++) {
+
+                    Play bean = playList.get(i);
+                    if(bean.OrderType.equalsIgnoreCase("Review")){
+                        playListForReview.add(bean);
+                    }
+                    if(bean.OrderType.equalsIgnoreCase("Perform")){
+                        playListForPerform.add(bean);
+                        playOrderList.add(bean.playOrderDetails);
+                    }
+
+                }
+
+                for (int i = 0; i < playOrderList.size(); i++) {
+                    if (playOrderList.get(i) != null) {
+                        PlayOrderDetails bean = playOrderList.get(i);
+                        Log.i("PlayOrderId", bean.PlayOrderId + "");
+                    }
+                }
+
+                listPlay.setAdapter(new ListPlayAdapterForReview(getActivity(), playListForReview));
+            }
+        });
 
     }
 
@@ -71,28 +144,173 @@ public class FragmentMyPlay extends Fragment implements RadioGroup.OnCheckedChan
     public void onResume() {
         super.onResume();
         rbGennemsyn.setChecked(true);
-       
+
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-        switch (checkedId){
+        switch (checkedId) {
 
             case R.id.rbBestilte:
-
-
-
+                listPlay.setAdapter(null);
+                listPlay.setAdapter(new ListPlayAdapterForPerform(getActivity(), playListForPerform,playOrderList));
                 break;
 
             case R.id.rbGennemsyn:
-
-
-
+                listPlay.setAdapter(null);
+                listPlay.setAdapter(new ListPlayAdapterForReview(getActivity(), playListForReview));
                 break;
+        }
+
+    }
+
+    // For Preview Tab
+    public class ListPlayAdapterForReview extends BaseAdapter {
+
+        Context context;
+
+        LayoutInflater inflater;
+
+        ArrayList<Play> playList;
+        ArrayList<PlayOrderDetails> playOrderDetailList;
+
+        public ListPlayAdapterForReview(Context context, ArrayList<Play> playList) {
+
+            this.context = context;
+
+            this.playList = playList;
+            this.playOrderDetailList = playOrderDetailList;
+        }
+
+        public int getCount() {
+
+            return playList.size();
 
         }
 
+        public Object getItem(int position) {
+            return playList.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+        class ViewHolder {
+
+            WMTextView txtTitle, txtAuther, txtDuration1, txtDuration2;
+
+
+        }
+
+        public View getView(final int position, View convertView,
+                            ViewGroup parent) {
+
+            final ViewHolder holder;
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item_myplay_preview, parent, false);
+                holder = new ViewHolder();
+                holder.txtTitle = (WMTextView) convertView.findViewById(R.id.txtPreviewTitle);
+                holder.txtAuther = (WMTextView) convertView.findViewById(R.id.txtPreviewAuther);
+                holder.txtDuration1 = (WMTextView) convertView.findViewById(R.id.txtPreviewDuration1);
+                holder.txtDuration2 = (WMTextView) convertView.findViewById(R.id.txtPreviewDuration2);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.txtTitle.setText(playList.get(position).Title);
+            holder.txtAuther.setText("Forfatter: "+playList.get(position).Author);
+            holder.txtDuration1.setText(playList.get(position).Duration+" min., "+playList.get(position).Age+" år");
+            holder.txtDuration2.setText("Musik: "+playList.get(position).Music+", "+playList.get(position).Actors+" medvirkende");
+
+            return convertView;
+
+        }
 
     }
+
+    // For Order tab
+    public class ListPlayAdapterForPerform extends BaseAdapter {
+
+        Context context;
+
+        LayoutInflater inflater;
+
+        ArrayList<Play> playList;
+        ArrayList<PlayOrderDetails> playOrderDetailList;
+
+        public ListPlayAdapterForPerform(Context context, ArrayList<Play> playList,ArrayList<PlayOrderDetails> playOrderDetailList) {
+
+            this.context = context;
+
+            this.playList = playList;
+            this.playOrderDetailList = playOrderDetailList;
+        }
+
+        public int getCount() {
+
+            return playList.size();
+
+        }
+
+        public Object getItem(int position) {
+            return playList.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+        class ViewHolder {
+
+            WMTextView txtTitle, txtAuther, txtDuration1, txtDuration2, txtNumberOfPerformance;
+
+
+        }
+
+        public View getView(final int position, View convertView,
+                            ViewGroup parent) {
+
+            final ViewHolder holder;
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item_myplay_order, parent, false);
+                holder = new ViewHolder();
+                holder.txtTitle = (WMTextView) convertView.findViewById(R.id.txtOrderTitle);
+                holder.txtAuther = (WMTextView) convertView.findViewById(R.id.txtOrderAuther);
+                holder.txtDuration1 = (WMTextView) convertView.findViewById(R.id.txtOrderDuration1);
+                holder.txtDuration2 = (WMTextView) convertView.findViewById(R.id.txtOrderDuration2);
+                holder.txtNumberOfPerformance = (WMTextView) convertView.findViewById(R.id.txtOrderNumberOfPerformance);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.txtTitle.setText(playList.get(position).Title);
+            if(playOrderDetailList.get(position).NumberOfAuditions.equalsIgnoreCase("True")){
+                holder.txtAuther.setText("Generalprøve: Ja");
+            } else {
+                holder.txtAuther.setText("Generalprøve: Nej");
+            }
+            SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
+            float performDateFirst=Float.parseFloat(playOrderDetailList.get(position).PerformDateFirst);
+            float performDateLast=Float.parseFloat(playOrderDetailList.get(position).PerformDateLast);
+            Date firstDate=new Date((long)performDateFirst);
+            Date lastDate=new Date((long)performDateLast);
+
+            holder.txtDuration1.setText("Første opførelse: "+format.format(firstDate));
+            holder.txtDuration2.setText("Sidste opførelse: "+format.format(lastDate));
+            holder.txtNumberOfPerformance.setText("Antal opførelser: "+playOrderDetailList.get(position).NumberOfPerformances);
+            return convertView;
+
+        }
+
+    }
+
+
 }
