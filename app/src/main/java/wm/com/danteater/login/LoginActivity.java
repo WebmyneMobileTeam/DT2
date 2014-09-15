5c28c72a736fbe7bb89dd4a59ac4dbb942157873
Nirav Kalola <niravkalola1991@gmail.com>
2014-09-15 16:50:10 +0530

    nirav changes

--- app/src/main/java/wm/com/danteater/login/LoginActivity.java
+++ app/src/main/java/wm/com/danteater/login/LoginActivity.java
@@ -1,11 +1,19 @@
 package wm.com.danteater.login;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -18,48 +26,53 @@ import com.mvnordic.mviddeviceconnector.DeviceSecurity;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import wm.com.danteater.R;
 import wm.com.danteater.app.BaseActivity;
 import wm.com.danteater.app.MyApplication;
+import wm.com.danteater.customviews.WMTextView;
 import wm.com.danteater.guide.GuideStartup;
-import wm.com.danteater.model.AppConstants;
+import wm.com.danteater.model.AdvancedSpannableString;
 import wm.com.danteater.my_plays.DrawerActivity;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements AdvancedSpannableString.OnClickableSpanListner {
 
-    private TextView txtLogin;
+
     DeviceSecurity m_device_security = null;
-
-
+    boolean shouldShowLoginView;
+    private LinearLayout loginView,noAccessView;
+    private RelativeLayout noNetworkView;
+    boolean isTeacherOrAdmin;
+    private WMTextView txtBottomLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         txtHeader.setText("Login");
-        txtLogin = (TextView) findViewById(R.id.txtLogin);
-        txtLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
+        // Alternative Views
+        loginView=(LinearLayout)findViewById(R.id.LoginView);
+        noAccessView=(LinearLayout)findViewById(R.id.noAccessView);
+        noNetworkView=(RelativeLayout)findViewById(R.id.noNetworkView);
 
-                if (isFirstTime()) {
+        txtBottomLabel=(WMTextView)findViewById(R.id.txtBottomLabel);
+        // get login value from setting activity
+        // true- automatic login
+        // false- manual login
+        SharedPreferences preferences = getSharedPreferences("settings", MODE_PRIVATE);
+        shouldShowLoginView = preferences.getBoolean("shouldShowLoginView", false);
 
-                    Intent i = new Intent(LoginActivity.this, GuideStartup.class);
-                    startActivity(i);
+        // automatic login is on
+        if (shouldShowLoginView == true) {
+            Intent i = new Intent(LoginActivity.this, DrawerActivity.class);
+            startActivity(i);
+            finish();
 
-
-                } else {
-
-                    Intent i = new Intent(LoginActivity.this, DrawerActivity.class);
-                    startActivity(i);
-                }
-
-
-            }
-        });
-
-
-        proceedLogin();
+        } else {
+            // automatic login is off
+            proceedLogin();
+        }
 
 
     }
@@ -73,21 +86,23 @@ public class LoginActivity extends BaseActivity {
         m_device_security.setApplicationCountryCode("IntoWords_dk");
         m_device_security.excludeLoginGroup("company");
         m_device_security.excludeLoginGroup("private");
-
+        // if automatic login is off, release the device registration
+        if (shouldShowLoginView == false) {
+            m_device_security.releaseDeviceRegistration();
+        }
+        // try to login
         m_device_security.doLogin("product.ios.da.intowords", R.id.fragment_layout);
 
     }
 
-    private boolean isFirstTime() {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        boolean ranBefore = preferences.getBoolean("RanBefore", false);
-        if (!ranBefore) {
-            // first time
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("RanBefore", true);
-            editor.commit();
-        }
-        return !ranBefore;
+
+    @Override
+    public void onSpanClick() {
+        Toast.makeText(this, "https://www.mv-nordic.com/dk/produkter/teater-aftale/", Toast.LENGTH_SHORT).show();
+        String url = "https://www.mv-nordic.com/dk/produkter/teater-aftale/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 
     private DeviceSecurity.DeviceSecurityListener device_security_listener = new DeviceSecurity.DeviceSecurityListener() {
@@ -106,15 +121,61 @@ public class LoginActivity extends BaseActivity {
             }
 
             String session_id = m_device_security.getMVSessionID(response.access_identifier);
+
             if (session_id == "" || session_id == null) {
+                // no internet connection
+                // shows no network view
+
+//                m_device_security.releaseDeviceRegistration();
+//
+//                AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
+//                alert.setTitle("Intet netværk");
+//                alert.setMessage("Login-serveren kunne ikke kontaktes. Tjek venligst dine netværksindstillinger, og prøv at logge ind igen.");
+//                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        m_device_security.releaseDeviceRegistration();
+//                    }
+//                });
+//                alert.show();
+//
+//                loginView.setVisibility(View.GONE);
+//                noAccessView.setVisibility(View.GONE);
+//                noNetworkView.setVisibility(View.VISIBLE);
+
+                //TODO
+                loginView.setVisibility(View.GONE);
+                noAccessView.setVisibility(View.VISIBLE);
+                noNetworkView.setVisibility(View.GONE);
+
+                loginView.setVisibility(View.GONE);
+                noAccessView.setVisibility(View.VISIBLE);
+                noNetworkView.setVisibility(View.GONE);
+                txtBottomLabel.setMovementMethod(LinkMovementMethod.getInstance());
+                txtBottomLabel.setText(Html.fromHtml("Ring til MV-Nordic på 6591 8022 eller klik <a href=\\\"https://www.mv-nordic.com/dk/produkter/teater-aftale\\\">her</a> for at læse mere."));
+
+
 
             } else {
-                callWebServicePost(session_id);
+
+                loginView.setVisibility(View.VISIBLE);
+                noAccessView.setVisibility(View.GONE);
+                noNetworkView.setVisibility(View.GONE);
+
+                // save session id in shared preferences
+                SharedPreferences preferences = getSharedPreferences("session_id", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("seesion_id", session_id);
+                editor.commit();
+
+                // post webservice
+                callWebServicePost(session_id, response);
             }
         }
 
 
-        private void callWebServicePost(String session_id) {
+        private void callWebServicePost(String session_id, final MVIDResponse mvid_response) {
 
 
             JSONObject params = new JSONObject();
@@ -125,8 +186,6 @@ public class LoginActivity extends BaseActivity {
                 params.put("session_id", session_id);
                 params.put("lookup_primary_group", true);
                 params.put("extract_userroles", true);
-
-
                 request_params.put("methodname", "whoami");
                 request_params.put("type", "jsonwsp/request");
                 request_params.put("version", "1.0");
@@ -135,15 +194,14 @@ public class LoginActivity extends BaseActivity {
                 e.printStackTrace();
             }
 
-
             JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, "https://mvid-services.mv-nordic.com/v2/UserService/jsonwsp", request_params, new Response.Listener<JSONObject>() {
 
                 @Override
                 public void onResponse(JSONObject jobj) {
 
                     String res = jobj.toString();
-                    Log.e("response: ",res+"");
-                    handlePostsList(res);
+                    Log.e("response: ", res + "");
+                    handlePostsList(res, mvid_response);
 
                 }
             }, new Response.ErrorListener() {
@@ -159,32 +217,120 @@ public class LoginActivity extends BaseActivity {
 
     };
 
-    private void handlePostsList(final String response) {
+    private void handlePostsList(final String response, final DeviceSecurity.DeviceSecurityListener.MVIDResponse mvid_response) {
 
         runOnUiThread(new Runnable() {
             public void run() {
 
                 try {
 
-                    Intent intent=null;
-                    BeanUserResult beanCustomerInfo =new GsonBuilder().create().fromJson(response, BeanUserResult.class);
-                    BeanUserInfo beanUserInfo=beanCustomerInfo.getBeanUserResult();
-                    BeanUser beanUser=beanUserInfo.getBeanUser();
+                    Intent intent = null;
+                    BeanUserResult beanCustomerInfo = new GsonBuilder().create().fromJson(response, BeanUserResult.class);
+                    BeanUserInfo beanUserInfo = beanCustomerInfo.getBeanUserResult();
+                    BeanUser beanUser = beanUserInfo.getBeanUser();
 
-                    Log.e("user_id: ",beanUser.getUserId()+"");
-                    Log.e("first_name: ",beanUser.getFirstName()+"");
-                    Log.e("last_name: ",beanUser.getLastName()+"");
-                    Log.e("primary_group: ",beanUser.getPrimaryGroup()+"");
-                    Log.e("roles: ",beanUser.getRoles()+"");
-
+                    Log.e("user_id: ", beanUser.getUserId() + "");
+                    Log.e("first_name: ", beanUser.getFirstName() + "");
+                    Log.e("last_name: ", beanUser.getLastName() + "");
+                    Log.e("primary_group: ", beanUser.getPrimaryGroup() + "");
+                    Log.e("roles: ", beanUser.getRoles() + "");
+                    isTeacherOrAdmin=checkTeacherOrAdmin(beanUser.getRoles());
 
                 } catch (JsonSyntaxException e) {
                     e.printStackTrace();
                 } catch (JsonIOException e) {
                     e.printStackTrace();
                 }
+
+                // store current user and domain in shared preferences
+
+
+                // if logged in user is teacher or admin and has no access
+                if (!mvid_response.has_access && isTeacherOrAdmin) {
+                    //TODO
+                    loginView.setVisibility(View.GONE);
+                    noAccessView.setVisibility(View.VISIBLE);
+                    noNetworkView.setVisibility(View.GONE);
+                    txtBottomLabel.setText(Html.fromHtml("Ring til MV-Nordic på 6591 8022 eller klik <a href=\\\"https://www.mv-nordic.com/dk/produkter/teater-aftale\\\">her</a> for at læse mere."));
+
+                } else {
+
+                    loginView.setVisibility(View.VISIBLE);
+                    noAccessView.setVisibility(View.GONE);
+                    noNetworkView.setVisibility(View.GONE);
+                    //TODO start login timer
+                    //TODO retrive school teacher
+                    //TODO retrive school classes
+                    //TODO update contents of navigation drawer
+
+                    // go to next screen
+                    if (isFirstTime()) { // show guide pages
+
+                        Intent i = new Intent(LoginActivity.this, GuideStartup.class);
+                        startActivity(i);
+                        finish();
+
+                    } else {    // show home screen
+
+                        Intent i = new Intent(LoginActivity.this, DrawerActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }
             }
         });
+    }
+
+    // check user is login first time or not
+    private boolean isFirstTime() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean ranBefore = preferences.getBoolean("RanBefore", false);
+        if (!ranBefore) {
+            // first time
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("RanBefore", true);
+            editor.commit();
+        }
+        return !ranBefore;
+    }
+
+    private boolean checkTeacherOrAdmin(ArrayList<String> roles) {
+        boolean isTeacher=false;
+        if(roles==null){
+            return false;
+        }
+        if(roles.size()==0){
+            return false;
+        }
+
+        for(String name: roles) {
+
+            if((name.equalsIgnoreCase("teacher")) || (name.equalsIgnoreCase("admin")) ) {
+                isTeacher=true;
+                break;
+            }
+        }
+        return isTeacher;
+    }
+
+
+    private boolean checkPupil(ArrayList<String> roles) {
+        boolean isPupil=false;
+        if(roles==null){
+            return false;
+        }
+        if(roles.size()==0){
+            return false;
+        }
+
+        for(String name: roles) {
+
+            if((name.equalsIgnoreCase("pupil")) ) {
+                isPupil=true;
+                break;
+            }
+        }
+        return isPupil;
     }
 
 }
--- app/src/main/java/wm/com/danteater/settings/SettingsFragment.java
+++ app/src/main/java/wm/com/danteater/settings/SettingsFragment.java
@@ -1,92 +1,60 @@
 package wm.com.danteater.settings;
 
-import android.app.Activity;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
 
 import wm.com.danteater.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class SettingsFragment extends Fragment {
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    private String mParam1;
-    private String mParam2;
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    private ToggleButton toggleAutomaticLogin;
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
+
     public SettingsFragment() {
-        // Required empty public constructor
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_settings, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
 
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
+        View rootView= inflater.inflate(R.layout.activity_settings, container, false);
+        SharedPreferences preferences = getActivity().getSharedPreferences("settings",getActivity().MODE_PRIVATE);
+        boolean shouldShowLoginView = preferences.getBoolean("shouldShowLoginView", false);
+        toggleAutomaticLogin=(ToggleButton)rootView.findViewById(R.id.settingAutomatiskeLogin);
+        toggleAutomaticLogin.setChecked(shouldShowLoginView);
+        toggleAutomaticLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean on = toggleAutomaticLogin.isChecked();
+                SharedPreferences preferences = getActivity().getSharedPreferences("settings",getActivity().MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                if (on) {
+                    editor.putBoolean("shouldShowLoginView", true);
+                    editor.commit();
+                } else {
+
+                    editor.putBoolean("shouldShowLoginView", false);
+                    editor.commit();
+                }
+            }
+        });
+        return rootView;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
 
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
 
 }
